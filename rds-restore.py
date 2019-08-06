8a0dae8407fd08379763edf663a2d8f17387b3ae
Felipe Ferreira <felipe@ipsense.com.br>
2019-08-06 15:09:19 -0300

    adds get_latest_snapshot, get_rds_description_ get_tags, delete_rds and create_rds

--- rds-restore.py
+++ rds-restore.py
@@ -1,8 +1,11 @@
 import boto3
 import json
-#import re
 import sys
 
+class RDSCopyException (Exception):
+    pass
+
+#Retrieves the rds snapshot list and return the latest one
 def get_latest_snapshot (rds_name):
 
     rds = boto3.Session(profile_name='poc', region_name='us-east-1').client('rds')
@@ -11,10 +14,30 @@ def get_latest_snapshot (rds_name):
         DBInstanceIdentifier=rds_name
     )['DBSnapshots']
 
+    if (len(snapshot_list) == 0):
+        raise RDSCopyException ("No snapshot for this db instance")
+
     snapshot_list = sorted(snapshot_list, key=lambda snapshot: snapshot['InstanceCreateTime'])
 
     return snapshot_list[-1]
 
+#Returns the description of a rds instance
+def get_rds_description (rds_name):
+
+    rds = boto3.Session(profile_name='poc', region_name='us-east-1').client('rds')
+
+    return rds.describe_db_instances(DBInstanceIdentifier=rds_name)['DBInstances'][0]
+
+#Return the tags of a RDS instance
+def get_tags (rds_arn):
+
+    rds = boto3.Session(profile_name='poc', region_name='us-east-1').client('rds')
+
+    tags = rds.list_tags_for_resource (ResourceName=rds_arn)['TagList']
+
+    return tags
+
+#Deletes a RDS
 def delete_rds (rds_name):
 
     rds = boto3.Session(profile_name='poc', region_name='us-east-1').client('rds')
@@ -24,77 +47,59 @@ def delete_rds (rds_name):
         SkipFinalSnapshot=True,
         DeleteAutomatedBackups=False
     )
-    return deleted_rds
+    rds.get_waiter('db_instance_deleted').wait(DBInstanceIdentifier=rds_name)
 
-def create_rds (snapshot_name):
+
+#Creates a RDS from the latest snapshot
+def create_rds (snapshot_name, rds_description):
 
     rds = boto3.Session(profile_name='poc', region_name='us-east-1').client('rds')
 
-    created_rds = rds.restore_db_instance_from_db_snapshot(
-        DBInstanceIdentifier=original_rds['DBInstanceIdentifier'],
-        DBSnapshotIdentifier=snapshot_name,
-        DBInstanceClass=original_rds['DBInstanceClass'],
-        Port=original_rds['Port'],
-        AvailabilityZone=original_rds['AvailabilityZone'],
-        DBSubnetGroupName=original_rds['DBSubnetGroupName'],
-        MultiAZ=original_rds['MultiAZ'],
-        PubliclyAccessible=original_rds['PubliclyAccessible'],
-        AutoMinorVersionUpgrade=original_rds['AutoMinorVersionUpgrade'],
-        LicenseModel='string',
-        DBName=original_rds['DBInstanceIdentifier'],
-        Engine=original_rds['Engine'],
-        Iops=original_rds['Iops'],
-        OptionGroupName='string',
-        Tags=[
-            {
-                'Key': 'string',
-                'Value': 'string'
-            },
-        ],
-        StorageType='string',
-        TdeCredentialArn='string',
-        TdeCredentialPassword='string',
-        VpcSecurityGroupIds=[
-            'string',
-        ],
-        Domain='string',
-        CopyTagsToSnapshot=True|False,
-        DomainIAMRoleName='string',
-        EnableIAMDatabaseAuthentication=True|False,
-        EnableCloudwatchLogsExports=[
-            'string',
-        ],
-        ProcessorFeatures=[
-            {
-                'Name': 'string',
-                'Value': 'string'
-            },
-        ],
-        UseDefaultProcessorFeatures=True|False,
-        DBParameterGroupName='string',
-        DeletionProtection=True|False
-    )
+    vpc_sec_groups_ids = []
 
+    for sg in rds_description['VpcSecurityGroups']:
+        vpc_sec_groups_ids.append(sg['VpcSecurityGroupId'])
+
+    new_rds = rds.restore_db_instance_from_db_snapshot(
+        DBInstanceIdentifier=rds_description['DBInstanceIdentifier'],
+        DBSnapshotIdentifier=snapshot_name,
+        DBInstanceClass=rds_description['DBInstanceClass'],
+        AvailabilityZone=rds_description['AvailabilityZone'],
+        DBSubnetGroupName=rds_description['DBSubnetGroup']['DBSubnetGroupName'],
+        MultiAZ=rds_description['MultiAZ'],
+        PubliclyAccessible=rds_description['PubliclyAccessible'],
+        AutoMinorVersionUpgrade=rds_description['AutoMinorVersionUpgrade'],
+        LicenseModel=rds_description['LicenseModel'],
+        Engine=rds_description['Engine'],
+        OptionGroupName=rds_description['OptionGroupMemberships'][0]['OptionGroupName'],
+        StorageType=rds_description['StorageType'],
+        VpcSecurityGroupIds=vpc_sec_groups_ids,
+        CopyTagsToSnapshot=rds_description['CopyTagsToSnapshot'],
+        EnableIAMDatabaseAuthentication=rds_description['IAMDatabaseAuthenticationEnabled'],
+        DBParameterGroupName=rds_description['DBParameterGroups'][0]['DBParameterGroupName'],
+        DeletionProtection=rds_description['DeletionProtection'],
+        Tags=get_tags(rds_description['DBInstanceArn'])
+    )
+    return new_rds
 
 def main ():
-
-    rds = boto3.Session(profile_name='poc', region_name='us-east-1').client('rds')
-
     rds_name = sys.argv[1]
-    
-    snapshot = get_latest_snapshot(rds_name)
-    
-    print ("Snapshot description")
-    print(json.dumps(snapshot, indent=4, sort_keys=True, default=str))
 
-    print(json.dumps(modify_rds (rds_name), indent=4, sort_keys=True, default=str))
+    try:
+        snapshot = get_latest_snapshot(rds_name)
 
-    original_rds = rds.describe_db_instances(
-        DBInstanceIdentifier=rds_name
-    )['DBInstances'][0]
+    except RDSCopyException as e:
+        print (e)
+        exit(1)
 
-    print ("RDS description")
-    print(json.dumps(original_rds, indent=4, sort_keys=True, default=str))
-    
+    rds_description = get_rds_description(rds_name)
+
+    delete_rds(rds_name)
+
+    new_rds = create_rds(snapshot['DBSnapshotIdentifier'], rds_description)
+
+    print ("New RDS")
+    print (json.dumps(new_rds, default=str, indent=4, sort_keys=True))
+
 if (__name__ == '__main__'):
     main()
